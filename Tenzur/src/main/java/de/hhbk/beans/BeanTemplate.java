98bc97076bed4320adb27c68811a6864d1c208c1
Robin Walter <robin.walter@mvise.de>
2023-11-03 10:45:36 +0100

    removed layout.css and implementes basic styles in style.css

--- Tenzur/src/main/java/de/hhbk/beans/BeanTemplate.java
+++ Tenzur/src/main/java/de/hhbk/beans/BeanTemplate.java
@@ -9,34 +9,6 @@ public abstract class BeanTemplate implements Serializable {
         super();
     }
 
-<<<<<<< HEAD
-    protected void setMessage(String comonentId, FacesMessage.Severity type, String header, String msg) {
-        FacesContext.getCurrentInstance().addMessage(comonentId, new FacesMessage(type, header, msg));
-=======
-public abstract class BeanTemplate implements Serializable {
-    //-------------------------------------------------------------------------
-    //  Constructor(s)
-    //-------------------------------------------------------------------------
-    public BeanTemplate() {
-        super();
->>>>>>> origin/implement-navigation
-    }
-
-<<<<<<< HEAD
-    protected void setErrorMessage(String header, String msg) {
-        setMessage(null, FacesMessage.SEVERITY_ERROR, header, msg);
-    }
-=======
->>>>>>> origin/implement-navigation
-
-<<<<<<< HEAD
-    protected void setInfoMessage(String header, String msg) {
-        setMessage(null, FacesMessage.SEVERITY_INFO, header, msg);
-    }
-=======
-    //-------------------------------------------------------------------------
-    //  Else ...
-    //-------------------------------------------------------------------------
     protected void setMessage(String comonentId, FacesMessage.Severity type, String header, String msg) {
         FacesContext.getCurrentInstance().addMessage(comonentId, new FacesMessage(type, header, msg));
     }
@@ -48,7 +20,4 @@ public abstract class BeanTemplate implements Serializable {
     protected void setInfoMessage(String header, String msg) {
         setMessage(null, FacesMessage.SEVERITY_INFO, header, msg);
     }
-
-
->>>>>>> origin/implement-navigation
 }
