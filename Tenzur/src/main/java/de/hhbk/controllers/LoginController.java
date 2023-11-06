25e789e7989016c7c32425086b00ea0625e54aee
Tibuarka <tobestyler@gmail.com>
2023-11-06 10:36:09 +0100

    Api Routen erstellt

--- Tenzur/src/main/java/de/hhbk/controllers/LoginController.java
+++ Tenzur/src/main/java/de/hhbk/controllers/LoginController.java
@@ -2,6 +2,7 @@ package de.hhbk.controllers;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +14,32 @@ public class LoginController extends HttpServlet{
  
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.sendRedirect("/");
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        try {
+            authenticate(request, response);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void authenticate(HttpServletRequest request, HttpServletResponse response)
+    throws Exception {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        System.out.println(username);
+        System.out.println(password);
+
+        // if (loginDao.validate(username, password)) {
+        //     RequestDispatcher dispatcher = request.getRequestDispatcher("login-success.jsp");
+        //     dispatcher.forward(request, response);
+        // } else {
+        //     throw new Exception("Login not successful..");
+        // }
     }
 }
