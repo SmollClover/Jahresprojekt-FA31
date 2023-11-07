be6bc106bbe2c7810322320bd5ce918e1d7922ab
Tibuarka <tobestyler@gmail.com>
2023-11-07 09:24:10 +0100

    Added ValidationManager

---
+++ Tenzur/src/main/java/de/hhbk/managers/ValidationManager.java
@@ -0,0 +1,15 @@
+package de.hhbk.managers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidationManager {
+    public Map<Boolean, String> validateLogin(String username, String password){
+        Map<Boolean, String> validation = new HashMap<>();
+        return validation;
+    }
+    public Map<Boolean, String> validateRegister(String username, String password){
+        Map<Boolean, String> validation = new HashMap<>();
+        return validation;
+    }
+}
