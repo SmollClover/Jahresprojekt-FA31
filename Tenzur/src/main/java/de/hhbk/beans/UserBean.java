43d56b15ec31dd05ec0e1f97a85871770fcb3c2b
Clover <39840298+SmollClover@users.noreply.github.com>
2023-11-07 13:27:21 +0100

    Load all Users in Bean

--- Tenzur/src/main/java/de/hhbk/beans/UserBean.java
+++ Tenzur/src/main/java/de/hhbk/beans/UserBean.java
@@ -1,21 +1,22 @@
 package de.hhbk.beans;
 
 import de.hhbk.entities.User;
+import de.hhbk.managers.DatabaseManager;
+import org.hibernate.Session;
 
-import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-// import javax.faces.bean.ManagedProperty;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.ServletContext;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Named(value = "user")
-// @SessionScoped
-@ManagedBean
+@SessionScoped
 public class UserBean extends BeanTemplate {
+    @Inject
+    private ServletContext ctx;
     private Collection<User> users = new ArrayList<User>();
 
     // @ManagedProperty("#{facesContext.externalContext.context}")
@@ -25,18 +26,12 @@ public class UserBean extends BeanTemplate {
         super();
     }
 
-
-    // @PostConstruct
-    // public void init() {
-    //     resetItem();
-    //     getItemList().add(new Drucker("880815", "Brother", "DCP 8490"));
-    //     getItemList().add(new Drucker("990815", "Canon", "MX-925"));
-    //     getItemList().add(new Drucker("222888", "HP", "DeskJet 9000"));
-    // }
-
     @PostConstruct
     public void init() {
-
+        DatabaseManager DB = (DatabaseManager) ctx.getAttribute("DB");
+        Session session = DB.getSessionFactory().openSession();
+        this.users = new User().getList(session);
+        session.close();
     }
 
     public String test() {
--- Tenzur/src/main/java/de/hhbk/entities/EntityTemplate.java
+++ Tenzur/src/main/java/de/hhbk/entities/EntityTemplate.java
@@ -3,6 +3,7 @@ package de.hhbk.entities;
 import org.hibernate.Session;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public class EntityTemplate<T> implements Serializable {
     private final Class<T> clazz;
@@ -28,5 +29,13 @@ public class EntityTemplate<T> implements Serializable {
         session.beginTransaction();
         session.delete(this);
         session.getTransaction().commit();
+    }
+
+    public Collection<T> getList(Session session) {
+        session.beginTransaction();
+        Collection<T> result = session.createQuery("FROM " + clazz.getName(), clazz).list();
+        session.getTransaction().commit();
+
+        return result;
     }
 }
