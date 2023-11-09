d382368237c3a22b4be7deb97c20b392261450fe
Clover <39840298+SmollClover@users.noreply.github.com>
2023-11-09 08:52:17 +0100

    Make Beans work on inheritance

--- Tenzur/src/main/java/de/hhbk/beans/BeanTemplate.java
+++ Tenzur/src/main/java/de/hhbk/beans/BeanTemplate.java
@@ -1,12 +1,69 @@
 package de.hhbk.beans;
 
+import de.hhbk.entities.EntityTemplate;
+import de.hhbk.managers.DatabaseManager;
+import org.hibernate.Session;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.servlet.ServletContext;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Collection;
 
-public abstract class BeanTemplate implements Serializable {
-    public BeanTemplate() {
+public class BeanTemplate<T extends EntityTemplate> implements Serializable {
+    private Class<T> clazz = null;
+    @Inject
+    ServletContext ctx;
+    private Collection<T> itemList = new ArrayList<T>();
+    private T item = null;
+
+    public BeanTemplate() {}
+
+    public BeanTemplate(Class<T> clazz) {
         super();
+        this.clazz = clazz;
+    }
+
+    public void loadItemList() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        DatabaseManager DB = (DatabaseManager) this.ctx.getAttribute("DB");
+        Session session = DB.getSessionFactory().openSession();
+        this.itemList = clazz.getDeclaredConstructor().newInstance().getList(session);
+        session.close();
+    }
+
+    public Collection<T> getItemList() {
+        return this.itemList;
+    }
+
+    public T getItem() {
+        return this.item;
+    }
+
+    public void setItem(T item) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        this.item = clazz.getDeclaredConstructor().newInstance();
+    }
+
+    public void resetItem() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        this.item = clazz.getDeclaredConstructor().newInstance();
+    }
+
+    public void addItem() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        if (this.itemList.contains(this.item)) return;
+
+        DatabaseManager DB = (DatabaseManager) ctx.getAttribute("DB");
+        Session session = DB.getSessionFactory().openSession();
+        this.item.save(session);
+        session.close();
+
+        this.itemList.add(this.item);
+        this.resetItem();
+    }
+
+    public void removeItem(T item) {
+        this.itemList.remove(this.item);
     }
 
     protected void setMessage(String comonentId, FacesMessage.Severity type, String header, String msg) {
--- Tenzur/src/main/java/de/hhbk/beans/BenutzerBean.java
+++ Tenzur/src/main/java/de/hhbk/beans/BenutzerBean.java
@@ -1,74 +1,25 @@
 package de.hhbk.beans;
 
 import de.hhbk.entities.Benutzer;
-import de.hhbk.managers.DatabaseManager;
-import org.hibernate.Session;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
-import javax.servlet.ServletContext;
-import java.util.ArrayList;
-import java.util.Collection;
 
 @Named(value = "benutzer")
 @SessionScoped
-public class BenutzerBean extends BeanTemplate {
-    @Inject
-    private ServletContext ctx;
-    private Collection<Benutzer> itemList = new ArrayList<Benutzer>();
-    private Benutzer item = null;
-
+public class BenutzerBean extends BeanTemplate<Benutzer> {
     public BenutzerBean() {
-        super();
-    }
-
-    public Collection<Benutzer> getUsers() {
-        return itemList;
+        super(Benutzer.class);
     }
 
     @PostConstruct
     public void init() {
-        this.loadItemList();
-    }
-
-    public void loadItemList() {
-        DatabaseManager DB = (DatabaseManager) ctx.getAttribute("DB");
-        Session session = DB.getSessionFactory().openSession();
-        this.itemList = new Benutzer().getList(session);
-        session.close();
-    }
-
-    public Collection<Benutzer> getItemList() {
-        return itemList;
-    }
-
-    public Benutzer getItem() {
-        return item;
-    }
-
-    public void setItem(Benutzer item) {
-        this.item = item;
-    }
-
-    public void resetItem() {
-        this.item = new Benutzer();
-    }
-
-    public void addItem() {
-        if (this.itemList.contains(this.item)) return;
-
-        DatabaseManager DB = (DatabaseManager) ctx.getAttribute("DB");
-        Session session = DB.getSessionFactory().openSession();
-        this.item.save(session);
-        session.close();
-
-        this.itemList.add(this.item);
-        this.resetItem();
-    }
-
-    public void removeItem(Benutzer item) {
-        this.itemList.remove(this.item);
+        try {
+            this.loadItemList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.setErrorMessage("Daten konnten nicht geladen werden", e.getMessage());
+        }
     }
 }
