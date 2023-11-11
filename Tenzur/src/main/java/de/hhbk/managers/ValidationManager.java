07c331b5849e0f0ba7a5d175fd1d2f164723abae
Tibuarka <tobestyler@gmail.com>
2023-11-11 18:12:15 +0100

    Added Documentation for ValidationManager

--- Tenzur/src/main/java/de/hhbk/managers/ValidationManager.java
+++ Tenzur/src/main/java/de/hhbk/managers/ValidationManager.java
@@ -4,11 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ValidationManager {
+    /**
+     * @param username the user input for the username
+     * @param password the user input for the password
+     * @return a map containing a status for success or failure and messages which of the inputs was invalid
+     */
     public Map<Boolean, String> validateLogin(String username, String password) {
         Map<Boolean, String> validation = new HashMap<>();
         return validation;
     }
 
+    /**
+     * @param username the user input for the username
+     * @param password the user input for the password
+     * @return a map containing a status for success or failure and messages which of the inputs was invalid
+     */
     public Map<Boolean, String> validateRegister(String username, String password) {
         Map<Boolean, String> validation = new HashMap<>();
         return validation;
